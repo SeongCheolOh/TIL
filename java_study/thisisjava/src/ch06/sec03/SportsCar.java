0878a14cf6c4bb2a1e5e0f2017e0f1dc31e626df
osc <osc0904@naver.com>
2023-01-25 03:46:32 +0900

    230124 study at home

--- java_study/thisisjava/src/ch06/sec03/SportsCar.java
+++ java_study/thisisjava/src/ch06/sec03/SportsCar.java
@@ -3,9 +3,6 @@ package ch06.sec03;
 public class SportsCar {//공개 클래스는
 						//소스파일 당 1개
 						//다른 패키지에서도 사용가능
-	
-	
-	
 
 }//end class
 
--- java_study/thisisjava/src/ch06/sec04/StudentExample.java
+++ java_study/thisisjava/src/ch06/sec04/StudentExample.java
@@ -7,11 +7,10 @@ public class StudentExample {
 		// 객체를 만드는 설계도보다는 단순 실행용
 		
 		Student s1 = new Student();
-		System.out.println("s1 변수가 Stydent 객체를 참조합니다");
+		System.out.println("s1 변수가 Student 객체를 참조합니다");
 		
 		Student s2 = new Student();
-		
-
+		//sec04 - Student 클래스가 생성되어있어서 여기서 클래스생성 따로 안해도 됨
 	}
 
 }
