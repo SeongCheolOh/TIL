da474fdd5b00cc8f3773486214692014e401f8c1
osc <osc0904@naver.com>
2023-02-15 13:51:08 +0900

    study at academy 230215

---
+++ java_study2/javaHeight04/src/javaHeight04/p659/Data2.java
@@ -0,0 +1,27 @@
+package javaHeight04.p659;
+
+public class Data2 {
+	
+	private String gradeClass;
+	private int studentCnt;
+	public Data2(String gradeClass, int studentCnt) {
+		super();
+		this.gradeClass = gradeClass;
+		this.studentCnt = studentCnt;
+	}
+	public String getGradeClass() {
+		return gradeClass;
+	}
+	public void setGradeClass(String gradeClass) {
+		this.gradeClass = gradeClass;
+	}
+	public int getStudentCnt() {
+		return studentCnt;
+	}
+	public void setStudentCnt(int studentCnt) {
+		this.studentCnt = studentCnt;
+	}
+	
+	
+
+}
